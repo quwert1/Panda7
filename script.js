b2e02f88831398d33c3c804f8fdf5cbae15aaf81
Vladislav Evdokimov <vlados.032.2018@gmail.com>
2023-03-16 19:26:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// var el = document.querySelector('#big-nav-menu').offsetWidth
+
+
+// alert(el)
+// matchMedia
+
+// const mediaQuery = window.matchMedia('(min-width: 768px)')
+// function handleTabletChange(e) {
+//   if (e.matches) {
+//     alert('Media Query Matched!')
+//   }
+// }
+// mediaQuery.addListener(handleTabletChange)
+// handleTabletChange(mediaQuery)
+
+
+// const show = window.document.querySelector('#big-nav-menu').matchMedia('(min-width: 268px)')
+// function handleTabletChange(e) {
+//   if (e.matches) {
+//     alert('Media Query Matched!')
+//   }
+// }
+// show.addListener(handleTabletChange)
+// handleTabletChange(show)
+
+
+
+// const mediaQuery = window.matchMedia('(max-width: 1200px)')
+// function handleTabletChange(e) {
+//   if (e.matches) {
+//     alert('Media Query Matched!')
+//   }
+// }
+// mediaQuery.addListener(handleTabletChange)
+// handleTabletChange(mediaQuery)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
